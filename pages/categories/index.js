4deeee3b01b3f8f5d8f4107e69039d108518b99f
dhaifan-dito <dhaifan.dito@sirclo.com>
2022-06-06 11:12:03 +0700

    feat(category):skeleton

--- pages/categories/index.js
+++ pages/categories/index.js
@@ -4,12 +4,31 @@ import Link from 'next/link';
 import { GET_CATEGORIES } from './schema';
 import { Button } from '@material-ui/core';
 import useStyles from '@styles/style';
+import Skeleton from '@material-ui/lab/Skeleton';
 
 function Categories() {
     const classes = useStyles()
     const { loading, error, data } = useQuery(GET_CATEGORIES);
 
-    if (loading) return <p>Please wait, Loading...</p>;
+    if (loading) return (
+        <div align="center" style={{margin:'20px'}}>
+        <Skeleton align='center' variant="rect" width={210} height={40} />
+        <div className='grid-4' style={{margin:'20px'}}>
+      <Skeleton variant="rect" width={210} height={50} align="center" style={{marginBottom:'10px'}}/>
+      <Skeleton variant="rect" width={210} height={50} align="center" style={{marginBottom:'10px'}}/>
+      <Skeleton variant="rect" width={210} height={50} align="center" style={{marginBottom:'10px'}}/>
+      <Skeleton variant="rect" width={210} height={50} align="center" style={{marginBottom:'10px'}}/>
+      <Skeleton variant="rect" width={210} height={50} align="center" style={{marginBottom:'10px'}}/>
+      <Skeleton variant="rect" width={210} height={50} align="center" style={{marginBottom:'10px'}}/>
+      <Skeleton variant="rect" width={210} height={50} align="center" style={{marginBottom:'10px'}}/>
+      <Skeleton variant="rect" width={210} height={50} align="center" style={{marginBottom:'10px'}}/>
+      <Skeleton variant="rect" width={210} height={50} align="center" style={{marginBottom:'10px'}}/>
+      <Skeleton variant="rect" width={210} height={50} align="center" style={{marginBottom:'10px'}}/>
+      <Skeleton variant="rect" width={210} height={50} align="center" style={{marginBottom:'10px'}}/>
+      <Skeleton variant="rect" width={210} height={50} align="center" style={{marginBottom:'10px'}}/>
+      </div>
+        </div>
+    );
     if (error) {return `Error : ${error.message}`}
 
     console.log(data.categories.items)
@@ -17,7 +36,7 @@ function Categories() {
   return (
     <div>
         <h2 align='center'>Queries</h2>
-        <div style={{display:'grid', gridTemplateColumns:'auto auto auto auto', margin:'20px'}}>
+        <div className="grid-4" style={{margin:'20px'}}>
         {
             data.categories.items && data.categories.items.map((item)=>{
                 return (
